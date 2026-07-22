656308a39e8530f4bf451ee21e4961aab49e06de
AbdulNasirWaheed <AbdulNasirWaheed@users.noreply.github.com>
2026-07-23 02:15:24 +0500

    Add files via upload

---
+++ Day-1/first-program.py
@@ -0,0 +1,23 @@
+print("Hello World")
+print("I am learning python")
+
+
+#Variables & Data Types
+name = "Ali" #string
+age = 20  #integer
+price = 19.99 #float
+is_enrolled = True #boolean
+course = "Python Programming"
+
+name= input("Enter your name: ")
+age= int(input("Enter your age: "))
+price= float(input("Enter the price: "))
+is_enrolled= bool(input("Are you enrolled: "))
+course= input("Enter the course: ")
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Course: {course}")
+print(f"Price: {price}")
+print(f"Enrolled: {is_enrolled}")
+
